c7ec7a4c8423c07f68c24bfbef98382179919e35
DevLogix <DevLogix@gmail.com>
2025-09-25 18:33:11 +0530

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCCICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
